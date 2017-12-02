64aebad68443dce431f16703feca6ec648b92c7e
huavanvu <huavanvu2812@gmail.com>
2017-12-02 22:48:13 +0700

    second commit

---
+++ Paint_Ribbon/Paint/Paint/RibbonIDs.h
@@ -0,0 +1,32 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define HomeTab 2 
+#define HomeTab_LabelTitle_RESID 60001
+#define ViewTab 3 
+#define ViewTab_LabelTitle_RESID 60002
+#define ID_MENU_NEW 4 
+#define ID_MENU_NEW_LabelTitle_RESID 60003
+#define ID_MENU_NEW_SmallImages_RESID 527
+#define ID_MENU_NEW_LargeImages_RESID 60004
+#define openMenu 5 
+#define openMenu_LabelTitle_RESID 60005
+#define ID_MENU_EXIT 6 
+#define ID_MENU_EXIT_LabelTitle_RESID 60006
+#define ID_MENU_EXIT_SmallImages_RESID 60007
+#define ID_MENU_EXIT_LargeImages_RESID 60008
+#define GroupShapes 7 
+#define GroupShapes_LabelTitle_RESID 60009
+#define ID_SHAPES_RECT 8 
+#define ID_SHAPES_RECT_LabelTitle_RESID 60010
+#define ID_SHAPES_RECT_SmallImages_RESID 60011
+#define ID_SHAPES_RECT_LargeImages_RESID 60012
+#define ID_SHAPES_ELIP 9 
+#define ID_SHAPES_ELIP_LabelTitle_RESID 60013
+#define ID_SHAPES_ELIP_SmallImages_RESID 60014
+#define ID_SHAPES_ELIP_LargeImages_RESID 60015
+#define fileMenu_LabelTitle_RESID 60016
